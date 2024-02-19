66b68cc81a718b7c55a1b03d2b446f362d355b18
Ronaldo Hauser Dias <duqueronaldo@icloud.com>
2024-02-19 16:25:58 -0300

    adicionada validação de 2 comentários em PostComments.test.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://m.media-amazon.com/images/I/51WIrUuNeML.__AC_SY300_SX300_QL70_ML2_.jpg">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import PostComments from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe('Teste para o componente PostComments', () => {
+  it('Deve renderizar o componente corretamente', () => {
+    render(<PostComments />);
+    expect(screen.getByText('Comentar')).toBeInTheDocument();
+  });
+
+  it('Deve inserir dois comentários', async () => {
+    render(<PostComments />);
+
+    const commentInput = screen.getByTestId('comment-input');
+    fireEvent.change(commentInput, { target: { value: 'Primeiro comentário' } });
+    const submitButton = screen.getByTestId('submit-button');
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
     });
+
+    fireEvent.change(commentInput, { target: { value: 'Segundo comentário' } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
+    });
+  });
 });
